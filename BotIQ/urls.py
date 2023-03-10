59aa13dd546749c3352f190b2ed16de0aa94c2db
JMVillota <jeffersonvillota@gmail.com>
2023-03-10 13:46:03 -0500

    Cambios

--- BotIQ/urls.py
+++ BotIQ/urls.py
@@ -17,8 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path( 'home/', include('home.urls') ),
-    
+    path( 'home/', include('home.urls') ),    
 ]
