e58df260f5db4732009acab1e4fedb6f12903866
JMVillota <jeffersonvillota@gmail.com>
2023-03-10 13:50:47 -0500

    camips

--- BotIQ/wsgi.py
+++ BotIQ/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BotIQ.settings')
 
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+    # Ejecuta la aplicación Django
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
